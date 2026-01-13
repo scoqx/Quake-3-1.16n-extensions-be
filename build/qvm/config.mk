88cc5bd1e9a4a60711fdec4ff9c085b045ea8f8d
diwoc <diwoc@proton.me>
2026-01-13 05:00:48 +0300

    tools

---
+++ build/qvm/config.mk
@@ -0,0 +1,36 @@
+PK3 = {X-Mod}.pk3
+
+export PATH := $(realpath ./tools):$(PATH)
+
+ifeq ($(PLATFORM),linux)
+
+Q3ASM_BIN = q3asm
+Q3LCC_BIN = q3lcc
+7Z_BIN = 7z
+
+endif #Linux
+
+ifdef MINGW
+
+Q3ASM_BIN = q3asm.exe
+Q3LCC_BIN = q3lcc.exe
+7Z_BIN = 7za.exe
+
+endif #MINGW
+
+Q3ASM = $(Q3ASM_BIN) -vq3 -r -m -v
+Q3LCC = $(Q3LCC_BIN) -DQ3_VM -S -Wf-g -I$(QADIR)
+7Z = $(7Z_BIN) u -tzip -mx=9 -mpass=8 -mfb=255 --
+
+basedir = ../..
+
+QADIR = $(basedir)/game
+CGDIR = $(basedir)/cgame
+UIDIR = $(basedir)/ui
+COMDIR = $(basedir)/qcommon
+
+QA_CFLAGS = -DQAGAME
+CG_CFLAGS = -DCGAME -I$(CGDIR)
+UI_CFLAGS = -DQ3UI -I$(UIDIR)
+
+include srcs.mk
